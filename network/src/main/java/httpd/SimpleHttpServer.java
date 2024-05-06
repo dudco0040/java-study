f84f372e9111c43e8561bf90545fade4beb309cd
BITCOM <BITCOM@BIT>
2024-05-07 00:19:02 +0900

    5/2 400, 404 error + chat#1 수업 실습 (복습 - 주석추가)

--- network/src/main/java/httpd/SimpleHttpServer.java
+++ network/src/main/java/httpd/SimpleHttpServer.java
@@ -13,13 +13,18 @@ public class SimpleHttpServer {
 		ServerSocket serverSocket = null;
 
 		try {
+			// 1. Create Server Socket 
 			serverSocket = new ServerSocket();
-
+			
+			// 2. Bind
 			serverSocket.bind( new InetSocketAddress( "0.0.0.0", PORT ) );
 			consolLog("starts...[" + PORT + "]");
 
 			while (true) {
+				// 3. Wait for connecting (accept)
 				Socket socket = serverSocket.accept();
+				
+				// 4. Delegate Processing Request 
 				new RequestHandler(socket).start();
 			}
 
