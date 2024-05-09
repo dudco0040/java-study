06a0124222c18d3c6caf223ea7f417a5d9757650
BITCOM <BITCOM@BIT>
2024-05-09 17:27:57 +0900

    과제 ing

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -1,28 +1,38 @@
 package prob1;
 
+import java.util.Arrays;
+
 public class Sort {
+	static int num1 = 0;
+	static int num2 = 0;
 	
 	public static void main(String[] arg) {
 	
 		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
 		int count =  array.length;
-		
-		System.out.println( "Before sort." );
-		
-		for (int i = 0; i < count; i++) {
-			System.out.print( array[ i ] + " " );
-		}
-		
-		//
-		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		//
 
 		
-		// 결과 출력
-		System.out.println( "\nAfter Sort." );
+		System.out.println( "Before sort." );
+		System.out.println(Arrays.toString(array));
+
 		
-		for (int i = 0; i < count; i++) {
-			System.out.print(array[i] + " ");
-		}		
+		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
+		for(int i = 0; i < count-1; i ++) {
+			for(int j = 0; j < count-1; j ++) {
+				if(array[i] < array[j]) {
+					change(array[i],array[j]);
+				}
+			}
+		}
+		System.out.println(num1);
+		// 결과 출력
+		System.out.println("\nAfter Sort." );
+		System.out.println(Arrays.toString(array));
+	}
+	
+	public static void change(int a, int b) {
+		int temp = a;
+		num1 = b;
+		num2 = temp;
 	}
 }
